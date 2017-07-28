2d4e19514a1e6ec901945e9bdfb62310ef74cb43
Philippe Krief <philippe.krief@eclipse.org>
2017-07-28 17:12:58 +0200

    Adding missing copyrights

--- ownCloudFileUpload.js
+++ ownCloudFileUpload.js
@@ -1,15 +1,13 @@
 /*******************************************************************************
- * Copyright (C) 2017 Orange.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *Copyright (C) 2017 ROMBIT.
+ *All rights reserved. This program and the accompanying materials
+ *are made available under the terms of the Eclipse Public License v1.0
+ *which accompanies this distribution, and is available at
+ *http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Rombit - initial implementation
- *
+ *Contributors:
+ *    ROMBIT - initial API and implementation
  ******************************************************************************/
-
 const path = require('path');
 var fs = require('fs');
 var d = require('debug')('OwnCloudFileUpload');
